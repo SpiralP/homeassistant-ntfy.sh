3ef7613596834f120ff54833de3624fd696c4a96
SpiralP <spiral.palmer@gmail.com>
2024-01-02 23:43:42 -0800

    convert to use username/password instead of token

--- custom_components/ntfy/notify.py
+++ custom_components/ntfy/notify.py
@@ -4,7 +4,8 @@ from typing import Any
 import voluptuous as vol
 from homeassistant.const import (
     CONF_URL,
-    CONF_TOKEN,
+    CONF_USERNAME,
+    CONF_PASSWORD,
     CONF_ICON
 )
 
@@ -20,41 +21,37 @@ from homeassistant.components.notify import (
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_URL): cv.url,
-    vol.Optional(CONF_TOKEN): cv.string,
-    vol.Optional(CONF_TOPIC): cv.string,
+    vol.Required(CONF_URL): cv.url,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_TOPIC): cv.string,
     vol.Optional(CONF_ICON): cv.url
 })
 _LOGGER = logging.getLogger(__name__)
 
 def get_service(hass, config, discovery_info=None):
-    url = config.get(CONF_URL) or 'https://ntfy.sh'
-    token = config.get(CONF_TOKEN) or ''
-    topic = config.get(CONF_TOPIC)
+    url = config[CONF_URL]
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+    topic = config[CONF_TOPIC]
     icon = config.get(CONF_ICON)
 
     _LOGGER.info('Service created')
 
-    return HassAgentNotificationService(hass, url, token, topic, icon)
+    return HassAgentNotificationService(hass, url, username, password, topic, icon)
 
 
 class HassAgentNotificationService(BaseNotificationService):
-    def __init__(self, hass, url, token, topic, icon):
+    def __init__(self, hass, url, username, password, topic, icon):
         self._url = url
-        self._token = token
+        self._username = username
+        self._password = password
         self._topic = topic
         self._icon = icon
         self._hass = hass
 
-        # if not self._url.endswith('/'):
-        #     self._url += '/'
-        # self._url += topic
-
-    def send_request(self, url, token, data):
-        headers = {}
-        if token != '':
-            headers = {'Authorization': 'Bearer ' + token}
-        return requests.post(url, headers=headers, json=data, timeout=10)
+    def send_request(self, url, username, password, data):
+        return requests.post(url, json=data, timeout=10, auth=(username, password))
 
     async def async_send_message(self, message: str, **kwargs: Any):
         title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
@@ -83,7 +80,7 @@ class HassAgentNotificationService(BaseNotificationService):
         _LOGGER.debug('Sending message to ntfy.sh: %s', payload)
 
         try:
-            response = await self.hass.async_add_executor_job(self.send_request, self._url, self._token, payload)
+            response = await self.hass.async_add_executor_job(self.send_request, self._url, self._username, self._password, payload)
             response.raise_for_status()
         except Exception as ex:
             _LOGGER.error('Error while sending ntfy.sh message: %s', ex)
